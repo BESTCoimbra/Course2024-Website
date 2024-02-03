e82e2275f9421b8380e3709dd6e17c01c6f6e489
Rodrigo <rborges11111@gmail.com>
2024-02-03 04:52:35 +0000

    Gallery - Falta Responsividade

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,38 @@ optionsPhone.forEach(option => {
     navbarPhone.style.display = "none";
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll('.best-gallery-buttons button');
+  const imageRows = document.querySelectorAll('.best-gallery-images .best-gallery-image-row, .best-gallery-images .best-gallery-image-row-2');
+  function hideAllImageRows() {
+    imageRows.forEach(row => {
+      row.style.display = 'none';
+    });
+  }
+  function showImageRow(year) {
+    const selectedRow = document.getElementById(`gallery-${year}`);
+    const selectedRow2 = document.getElementById(`gallery-${year}-2`);
+    if (selectedRow) {
+      selectedRow.style.display = 'flex';
+    }
+    if (selectedRow2) {
+      selectedRow2.style.display = 'flex';
+    }
+  }
+  hideAllImageRows();
+  showImageRow('2023');
+  buttons.forEach(button => {
+    if (button.getAttribute('data-year') === '2023') {
+      button.classList.add('clicked');
+    }
+    button.addEventListener('click', function () {
+      const year = this.getAttribute('data-year');
+      hideAllImageRows();
+      showImageRow(year);
+      buttons.forEach(btn => btn.classList.remove('clicked'));
+      this.classList.add('clicked');
+    });
+  });
+});
+
+
